7e533801f23a7c679bbf97d89d4f5bc67fd02d76
Mathijs Van den Broeck <mathijsvandenbroeck@gmail.com>
2015-10-08 11:33:31 +0200

    updated config files to prepare for environment file.

--- config/app.php
+++ config/app.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'debug' => env('APP_DEBUG', true),
+    'debug' => env('APP_DEBUG', false),
 
     /*
     |--------------------------------------------------------------------------
@@ -78,7 +78,7 @@ return [
     |
     */
 
-    'key' => env('APP_KEY', 'p5zeBTa2J44VMqWx86ygzJFwHSJYRjEW'),
+    'key' => env('APP_KEY', 'SomeRandomString'),
 
     'cipher' => 'AES-256-CBC',
 
