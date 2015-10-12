2e1d36a1e478353c23d046974fb802e4fcd2e207
Marijn Rutten <Marijn@Marijns-MacBook-Pro.local>
2015-10-12 11:39:59 +0200

    installed bootstrap with composer failed

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>App Name - @yield('title')</title>
+
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+
+
+<div class="container">
+    @yield('content');
+</div>
+
+</body>
+</html>
