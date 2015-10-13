f9b0af98a61cb93850750cd6c6a5ae42c9caa01b
MatthiasChristiaens <r0431354@student.thomasmore.be>
2015-10-13 21:07:17 +0200

    password reset routes

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -20,3 +20,11 @@ Route::post('update', 'ProfileController@postProfile');
 Route::get('/login', 'Auth\AuthController@getLogin');
 Route::post('/login', 'Auth\AuthController@postLogin');
 Route::get('/logout', 'Auth\AuthController@getLogout');
+
+// Password reset link request routes...
+Route::get('password/email', 'Auth\PasswordController@getEmail');
+Route::post('password/email', 'Auth\PasswordController@postEmail');
+
+// Password reset routes...
+Route::get('password/reset/{token}', 'Auth\PasswordController@getReset');
+Route::post('password/reset', 'Auth\PasswordController@postReset');
