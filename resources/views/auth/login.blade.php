fa5780dc371dd0876f05db603986555beba128f0
Mathijs Van den Broeck <mathijsvandenbroeck@gmail.com>
2015-10-13 12:59:46 +0200

    added basic error messages to login form

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -26,4 +26,14 @@
     </div>
 </form>
 
+@if (count($errors) > 0)
+    <div class="alert alert-danger">
+        <ul>
+            @foreach ($errors->all() as $error)
+                <li class="form-group has-error">{{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
+@endif
+
 @stop
