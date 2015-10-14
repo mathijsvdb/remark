7e882ecec6b754ebf87ac6c200b5cc6276faf734
MatthiasChristiaens <r0431354@student.thomasmore.be>
2015-10-14 15:50:45 +0200

    reset password

--- resources/views/emails/password.blade.php
+++ resources/views/emails/password.blade.php
@@ -1,1 +1,6 @@
-Click here to reset your password: {{ url('password/reset/'.$token) }}
+Click here to reset your password:
+
+<a href="{{ url('password/reset/'.$token) }}">Reset my password</a>
+
+This link expires in 24 hours
+
