37b8e90c8bde43ccd7f753d3ea692eafd360e0d2
jonasreymen <jonasr001@hotmail.com>
2015-10-21 13:07:03 +0200

    image upload

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -5,8 +5,11 @@ namespace App\Http\Controllers;
 use App\Project;
 use App\User;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Input;
 use Request;
 use App\Http\Requests;
+use Validator;
+use Redirect;
 use App\Http\Controllers\Controller;
 
 class ProjectController extends Controller
@@ -22,19 +25,58 @@ class ProjectController extends Controller
     }
 
     public function postAddProject() {
+        $title = Request::input('title');
+        $body = Request::input('body');
+        $tags = Request::input('tags');
+        $image = Input::file('fileToUpload');
+        $destinationPath = 'uploads'; // upload path
+
+        //rules
+        $file = array(
+            'fileToUpload' => $image,
+            'title' => $title,
+            'body' => $body,
+            'tags' => $tags,
+        );
+        $rules = array(
+            'fileToUpload' => 'required|image',
+            'title' => 'required',
+            'body' => 'required',
+            'tags' => 'required',
+        );
+
+        //validator
+        $validator = Validator::make($file, $rules);
+
+        //if validator fails
+        if ($validator->fails()) {
+            // send back to the page with the input data and errors
+            return Redirect::to('/projects/add')->withInput()->withErrors($validator);
+        }
+
+        //if input is a file upload to destination /uploads
+        //else redirect to view with error message
+        if($image->isValid()){
+            $extension = $image->getClientOriginalExtension(); // getting image extension
+            $fileName = Auth::user()->username . '_' . rand(11111,99999).'.'.$extension; // renaming image
+            Input::file('fileToUpload')->move($destinationPath, $fileName);
+        } else {
+            Session::flash('error', 'uploaded file is not valid');
+            return Redirect::to('/projects/add');
+        }
+
         $project = new Project;
 
-        $project->title = Request::input('title');
-        $project->body = Request::input('body');
-        $project->tags = Request::input('tags');
-        $project->img = Request::input('fileToUpload');
+        $project->title = $title;
+        $project->body = $body;
+        $project->tags = $tags;
+        $project->img = $fileName;
 
         $project->user_id = Auth::id();
         //$project->user_id = 1;
         //$table->increments('id');
 
         $project->save();
-
         return redirect('/projects');
     }
 
--- resources/views/addProject.blade.php
+++ resources/views/addProject.blade.php
@@ -2,44 +2,51 @@
 
 @section("content")
 
+    @if (count($errors) > 0)
+        <div class="alert alert-danger">
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li class="form-group has-error">{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
+
     <div class="col-sm-6 col-md-4 col-md-offset-4">
-<form action="/projects/add" method="post" role="form">
-    {!! csrf_field() !!}
+        <form action="/projects/add" method="post" enctype="multipart/form-data">
+            {!! csrf_field() !!}
 
-    <div class="form-group">
-        <label for="title">Project title</label><br>
-        <input type="text" name="title" style="color:black"> <br>
+            <div class="form-group">
+                <label for="title">Project title</label><br>
+                <input type="text" name="title" style="color:black" value={!! old('title') !!}> <br>
+            </div>
+
+            <div class="form-group">
+                <label for="body">Project description:</label><br>
+                <input type="text" name="body" style="color:black" value={!! old('body') !!}><br>
+            </div>
+
+            <div class="form-group">
+            <label for="tags">Add image tags:</label><br>
+                <input type="checkbox" name="tags" value="web">Web Design
+                <input type="checkbox" name="tags" value="illustrator">Illustrator
+                <input type="checkbox" name="tags" value="photoshop">Photoshop
+                <input type="checkbox" name="tags" value="mobile">Mobile Design<br>
+                <input type="checkbox" name="tags" value="logo">Logo
+                <input type="checkbox" name="tags" value="poster">Poster
+                <input type="checkbox" name="tags" value="material">Material Design
+                <input type="checkbox" name="tags" value="branding">Branding <br>
+            </div>
+
+            <div class="form-group">
+            Select image to upload:
+            <input type="file" name="fileToUpload" id="fileToUpload" value={!! old('fileToUpload') !!}>
+            </div>
+
+            <div class="form-group">
+                <button type="submit">Add project</button>
+            </div>
+
+        </form>
     </div>
-
-    <div class="form-group">
-        <label for="body">Project description:</label><br>
-        <input type="text" name="body" style="color:black"><br>
-    </div>
-
-    <div class="form-group">
-    <label for="tags">Add image tags:</label><br>
-        <input type="checkbox" name="tags" value="web">Web Design
-        <input type="checkbox" name="tags" value="illustrator">Illustrator
-        <input type="checkbox" name="tags" value="photoshop">Photoshop
-        <input type="checkbox" name="tags" value="mobile">Mobile Design<br>
-        <input type="checkbox" name="tags" value="logo">Logo
-        <input type="checkbox" name="tags" value="poster">Poster
-        <input type="checkbox" name="tags" value="material">Material Design
-        <input type="checkbox" name="tags" value="branding">Branding <br>
-    </div>
-
-    <div class="form-group">
-    Select image to upload:
-    <input type="file" name="fileToUpload" id="fileToUpload">
-    </div>
-
-    <div class="form-group">
-        <button type="submit">Add project</button>
-    </div>
-
-</form>
-</div>
-
-
-
 @stop
