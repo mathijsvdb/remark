7da393b7949ede1c4a53bb556c5d1afd79bb1623
tommooredotbe <mooretom@outlook.com>
2015-10-21 13:28:50 +0200

    partly updated projects view

--- resources/views/projects.blade.php
+++ resources/views/projects.blade.php
@@ -1,12 +1,28 @@
+@extends("layouts.master")
+
+@section("content")
+
+
 <h1>Projects</h1>
 
 <a href="/projects/add">+ Add a project</a>
 
-
+<div class="row">
+    <div class="gallery">
 <ul>
     @foreach($projects as $project)
-        <li>
-            <a href="/projects/{{ $project->id }}">{{ $project->title }}</a>
-        </li>
+        <div class="col-md-4">
+            <li class="projectslist">
+                <a class="projecttitle" href="/projects/{{ $project->id }}">{{ $project->title  }}</a><br>
+                <a class="btn btn-default glyphicon glyphicon-hand-up"></a><a class="btn btn-default glyphicon glyphicon-star"></a>
+                <img src="{{ $project->img  }}" alt=""><br>
+                tag: <span>{{ $project->tags  }}</span>
+            </li>
+        </div>
     @endforeach
 </ul>
+
+        </div>
+    </div>
+
+@stop
