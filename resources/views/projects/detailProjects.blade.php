60eec87038752b289c4b39896d88a768f6efaba5
Marijn Rutten <Marijn@Marijns-MacBook-Pro.local>
2015-10-28 20:56:43 +0100

    migrations kleuren in database, bij uploaden worden ge kleurcodes ook opgeslagen in de databank

--- resources/views/projects/detailProjects.blade.php
+++ resources/views/projects/detailProjects.blade.php
@@ -1,18 +1,20 @@
 @extends("layouts.master")
 
 @section("content")
-    <h2>{!! $project['title'] !!}</h2>
-    <p>by <a href="/profile/{!! $user['id'] !!}"><strong>{!! $user['firstname'] . " " . $user['lastname'] !!}</strong></a>{!! " " . $project['created_at'] !!}</p>
-    <img style="width: 150px; height: 120px;" src="/uploads/{!! $project['img'] !!}" alt="">
+    <div class="detail_container">
+        <h2>{!! $project['title'] !!}</h2>
+        <p>by <a href="/profile/{!! $user['id'] !!}"><strong>{!! $user['firstname'] . " " . $user['lastname'] !!}</strong></a>{!! " " . $project['created_at'] !!}</p>
+        <img style="width: 150px; height: 120px;" src="/uploads/{!! $project['img'] !!}" alt="">
 
-    <div>
-        <span class="glyphicon glyphicon-tint"></span>
+        <div>
+            <span class="glyphicon glyphicon-tint"></span>
 
-        @for($i=0; $i<count($palette); $i++)
-            <a style="display: block;background-color: {!! $palette[$i] !!}; width: 30px; height: 10px;"></a>
-        @endfor
+            @for($i=0; $i<count($palette); $i++)
+                <a style="display: block;background-color: {!! $palette[$i] !!}; width: 30px; height: 10px;"></a>
+            @endfor
+        </div>
+
+        <p>{!! $project['body'] !!}</p>
+        <a href="/">{!! $project['tags'] !!}</a>
     </div>
-
-    <p>{!! $project['body'] !!}</p>
-    <a href="/">{!! $project['tags'] !!}</a>
 @stop
