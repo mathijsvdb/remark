f3cdfb1a63aa267c7e496e13f5869707e465bbbe
Marijn Rutten <Marijn@Marijns-MacBook-Pro.local>
2015-10-30 16:50:27 +0100

    side notificartions

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -21,7 +21,7 @@ Route::get('/','frontpageController@frontpage');
 Route::get('profile/{id}','ProfileController@profile');
 Route::get('update','ProfileController@updateProfile');
 Route::post('update', 'ProfileController@postProfile');
-Route::get('/profile/activity','UserActivityController@showAllActivity');
+Route::get('/profile/{id}/activity','UserActivityController@showAllActivity');
 
 
 // Registration routes...
