ddf06742099bb189de1127cb1464dba6d26833f1
jonasreymen <jonasr001@hotmail.com>
2015-11-18 16:03:25 +0100

    behat installed, behat.yml in orde, eerste test werkt

---
+++ features/bootstrap/FeatureContext.php
@@ -0,0 +1,19 @@
+<?php
+
+
+use Behat\MinkExtension\Context\MinkContext;
+
+
+/**
+ * Features context.
+ */
+class FeatureContext extends MinkContext
+{
+    /**
+     * Initializes context.
+     * Every scenario gets its own context object.
+     */
+    public function __construct()
+    {
+    }
+}
---
+++ features/test.feature
@@ -0,0 +1,7 @@
+Feature: test
+  This is a standard behat test to see if the homepage shows the right notifications
+
+##TODO
+  Scenario: Test scenario test 1 2 3 4 5 6 7 8 9 10
+    Given I am on "/"
+    Then I should see "DO YOU WANT TO MAKE THE MOST OF YOUR DESIGN?"
