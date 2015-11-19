371eceac82cc1314f76b2e6c62cdb062e26e0f7d
Mathijsvdb <mathijsvandenbroeck@gmail.com>
2015-11-19 17:17:53 +0100

    updated migrations to cascade on delete with foreign keys

--- database/migrations/2015_10_22_121044_create_favorites_table.php
+++ database/migrations/2015_10_22_121044_create_favorites_table.php
@@ -15,9 +15,9 @@ class CreateFavoritesTable extends Migration
         Schema::create('favorites', function (Blueprint $table) {
             $table->increments('id');
             $table->integer('user_id')->unsigned();
-            $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
             $table->integer('project_id')->unsigned();
-            $table->foreign('project_id')->references('id')->on('projects');
+            $table->foreign('project_id')->references('id')->on('projects')->onDelete('cascade');
         });
     }
 
