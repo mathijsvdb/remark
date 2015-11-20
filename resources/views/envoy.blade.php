c4bef7b70e5b67cafcf3a35815342702eee877d7
tommooredotbe <mooretom@outlook.com>
2015-11-20 12:49:13 +0100

    envoy blade

---
+++ resources/views/envoy.blade.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: tommoore
+ * Date: 20/11/15
+ * Time: 11:28
+ */
+
+@servers( ['production' => ‘deploybot@139.162.159.6’ ])
+@task('deploy', ['on' => 'production'])
+cd /home/remark/
+php artisan down
+git reset --hard HEAD
+git pull origin master
+php composer.phar dump-autoload
+php artisan migrate --force
+php artisan up
+@endtask
