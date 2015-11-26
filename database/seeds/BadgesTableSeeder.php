6880d054feba5f73308d301d5c60eacd1da7beae
Mathijsvdb <mathijsvandenbroeck@gmail.com>
2015-11-26 12:10:30 +0100

    fixed spelling mistake

--- database/seeds/BadgesTableSeeder.php
+++ database/seeds/BadgesTableSeeder.php
@@ -27,7 +27,7 @@ class BadgesTableSeeder extends Seeder
             ],
             [
                 'badge_title' => 'Helping hand',
-                'badge_description' => "You've given feedback 5 times on one project",
+                'badge_description' => "You've given feedback 5 times on one project.",
                 'badge_img' => 'badges_1.png'
             ],
             [
