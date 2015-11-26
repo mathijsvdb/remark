227efabd72ff43aa544486e0795478332ec4d779
Mathijsvdb <mathijsvandenbroeck@gmail.com>
2015-11-26 11:59:41 +0100

    added badges migration + seeder'

---
+++ database/migrations/2015_11_26_103107_create_badges_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBadgesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('badges', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('badge_title');
+            $table->string('badge_description');
+            $table->string('badge_image');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('badges');
+    }
+}
---
+++ database/seeds/BadgesTableSeeder.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class BadgesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('badges')->delete();
+
+        $badges = [
+            [
+                'badge_title' => 'First timer',
+                'badge_description' => "You've feedback within 2 hours of registration.",
+                'badge_img' => 'badges_0.png'
+            ],
+            [
+                'badge_title' => 'Founding member',
+                'badge_description' => 'You are one of the first 100 members.',
+                'badge_img' => 'badges_1.png'
+            ],
+            [
+                'badge_title' => 'Helping hand',
+                'badge_description' => "You've given feedback 5 times on one project",
+                'badge_img' => 'badges_1.png'
+            ],
+            [
+                'badge_title' => 'Look at me!',
+                'badge_description' => 'You uploaded your first project.',
+                'badge_img' => 'badges_3.png'
+            ],
+            [
+                'badge_title' => "I'm a regular",
+                'badge_description' => "You've logged in for 3 consecutive days.",
+                'badge_img' => 'badges_4.png'
+            ],
+        ];
+
+        DB::table('badges')->insert($badges);
+    }
+}
