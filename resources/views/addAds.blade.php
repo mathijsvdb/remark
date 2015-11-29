2258a4e09067502464af8662f508c62f34b93f9a
Marijn Rutten <Marijn@Marijns-MacBook-Pro.local>
2015-11-29 18:25:40 +0100

    advertising add styles

--- resources/views/addAds.blade.php
+++ resources/views/addAds.blade.php
@@ -2,36 +2,37 @@
 
 @section("content")
 
-    @if (count($errors) > 0)
-        <div class="alert alert-danger">
-            <ul>
-                @foreach ($errors->all() as $error)
-                    <li class="form-group has-error">{{ $error }}</li>
-                @endforeach
-            </ul>
-        </div>
-    @endif
-
-    <div class="advertising_container">
+    <div class="advertising_container content-box row">
+        @if (count($errors) > 0)
+            <div class="alert alert-danger">
+                <ul>
+                    @foreach ($errors->all() as $error)
+                        <li class="form-group has-error">{{ $error }}</li>
+                    @endforeach
+                </ul>
+            </div>
+        @endif
         <form class="form-horizontal" method="post" action="/advertising/add" role="form" enctype="multipart/form-data">
             {!! csrf_field() !!}
             <h2>Your advertising</h2>
             <div class="form-group">
-                <label for="title" class="col-sm-3 control-label">Information to display</label>
-                <div class="col-sm-9">
+
+                <div class="col-sm-12">
+                    <label for="title" class="control-label">Title:</label>
                     <input type="text" id="title" placeholder="Title" name="title" class="form-control" autofocus required="required">
+                    <label for="url">Url to website:</label>
                     <input type="text" id="url" placeholder="http://" name="url" class="form-control" autofocus required="required">
                     <span class="help-block">Example: http://example.com</span>
                     <label for="startDate">Date :</label>
-                    <input name="data-picker" id="date-picker" type="date" class="date-picker" />
+                    <input name="data-picker" id="input" type="date" class="date-picker" />
 
                 </div>
 
             </div>
             <div class="form-group">
-                <div class="col-sm-9">
-                    <label for="inputFileupload">Advertising image:</label>
-                    <input type="file" style="margin-top: 15px;" name="fileToUpload" id="fileToUpload">
+                <div class="col-sm-12">
+                    <label for="fileToUpload">Image:</label>
+                    <input type="file" name="fileToUpload" id="fileToUpload">
                     <span class="help-block">Image has to be width 200 and height 150</span>
                 </div>
 
