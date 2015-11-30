80cecbcfa254d2e2e1eae63a465e5d94c0aedac7
Marijn Rutten <Marijn@Marijns-MacBook-Pro.local>
2015-11-30 14:09:09 +0100

    developer not styled yet

--- resources/views/developer.blade.php
+++ resources/views/developer.blade.php
@@ -3,12 +3,13 @@
 @section("content")
 
     <div class="api_container">
-        <h2>Get API key here</h2>
-        <p>You can ge</p>
+        <h2>Get API key here v1</h2>
+        <p>You can get you api key here</p>
+        <p>there are 2 different api's avilable. één die je de meest <strong>populaire</strong> items weergeeft en een andere die geeft wat meer <strong>details</strong>.</p>
         <button id="generate_API_KEY">Generate Key</button>
         <div class="user_api_key_container">
 
-            <ul class="row"> <!-- lus hier keys uit-->
+            <ul class="row"> <!-- lus hier keys uit die gelinkt zijn aan de gebruiker.-->
                 <li>
                     key key key key key
                 </li>
