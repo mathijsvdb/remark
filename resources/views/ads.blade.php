5d5d3b4f75aab5e48d5404630b127152f4c2687c
jonasreymen <jonasr001@hotmail.com>
2015-12-02 14:36:57 +0100

    stripe fix

--- resources/views/ads.blade.php
+++ resources/views/ads.blade.php
@@ -3,7 +3,7 @@
 @section("content")
 
     <div class="adver_container">
-        <h2>Your advertising's</h2> <a class="" href="http://remark.dev/advertising/add">Add +</a>
+        <h2>Your advertising's</h2> <a class="" href="/advertising/add">Add +</a>
 
         <ul class="row">
             @foreach($myAds as $ad)
