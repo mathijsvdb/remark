f70c789c9ca160f6973cfdc884e071e4f6527708
Marijn Rutten <Marijn@Marijns-MacBook-Pro.local>
2015-12-06 14:55:40 +0100

    footer width too big fix

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -1,5 +1,5 @@
 
-    <nav class="navbar navbar-default">
+    <nav class="navbar navbar-default navbar_content">
         <div class="container-fluid">
             <!-- Brand and toggle get grouped for better mobile display -->
             <div class="navbar-header">
