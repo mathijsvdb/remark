1efa10a57ae60253429cd5c77de70f90292d5e86
Marijn Rutten <Marijn@Marijns-MacBook-Pro.local>
2015-12-06 20:28:36 +0100

    fixed register page

--- resources/views/frontpage.blade.php
+++ resources/views/frontpage.blade.php
@@ -15,7 +15,7 @@
                 <h1>Do you want to make the most of your design?</h1>
                 <hr>
                 <p>At remark <b> students </b> like you post a design, then other students, start giving other students feedback on the design so that the students can learn from each other!</p>
-                <a href="{{ url('/registreren') }}" class="btn btn-primary animation-bounce">Start here, It's FREE!</a>
+                <a href="{{ url('/register') }}" class="btn btn-primary animation-bounce">Start here, It's FREE!</a>
             </div>
         </div>
     </header>
