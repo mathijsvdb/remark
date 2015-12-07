4eac95ea8144393ff36d64996bc9e86050969e20
MatthiasChristiaens <r0431354@student.thomasmore.be>
2015-12-07 20:30:58 +0100

    own activity notification added

--- resources/views/UserActivity.blade.php
+++ resources/views/UserActivity.blade.php
@@ -2,58 +2,40 @@
 
 @section("content")
 
-    <div class="container_filter form-group">
-        <h2>Filter</h2>
-        <form id="notification_filter" method="post" action="/">
-            <input type="hidden" name="_token" value="{{ csrf_token() }}">
-            <span><input type="checkbox" class="notification_checkbox" id="all" value="all" checked="checked" class="notifications" name="all">all</span>
-            <span><input type="checkbox" class="notification_checkbox" id="likes" value="likes" class="notifications" name="likes">like</span>
-            <span><input type="checkbox" class="notification_checkbox" id="favorite" value="favorite" class="notifications" name="favorite">favorite</span>
-            <span><input type="checkbox" class="notification_checkbox" id="comment" value="comment" class="notifications" name="comment">comment</span>
-
-        </form>
-    </div>
-
     <div class="activity">
 
-        <ul id="activity">
-        </ul>
-
         <h2>Your user activity</h2>
-        <a href="#">New notifications <span class="badge aantalnotificaties">3</span></a>
 
-        <div class="list-group notificationlist">
-            <!-- Genereer zo een list item in controller-->
-            <a href="#" class="list-group-item active" id="">
-                <h4 class="list-group-item-heading"><i class="fa fa-thumbs-up"></i> <span class="notification_from_who" id="">Joske</span> <span class="notification_whathappend" id="">liked your picture</span></h4>
-                <p class="list-group-item-text">Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.</p>
-            </a>
-            <a href="#" class="list-group-item active" id="">
-                <h4 class="list-group-item-heading"><i class="fa fa-heart"></i> <span class="notification_from_who" id="">Tom</span> <span class="notification_whathappend" id="">favorited your picture</span></h4>
-                <p class="list-group-item-text">Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.</p>
-            </a>
-            <a href="#" class="list-group-item active" id="">
-                <h4 class="list-group-item-heading"><i class="fa fa-thumbs-up"></i> <span class="notification_from_who" id="">Marijn</span> <span class="notification_whathappend" id="">liked your picture</span></h4>
-                <p class="list-group-item-text">Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.</p>
-            </a>
-            <a href="#" class="list-group-item">
-                <h4 class="list-group-item-heading" id=""><i class="fa fa-thumbs-up"></i> <span class="notification_from_who" id="">Koen Pellegrims</span> <span class="notification_whathappend" id="">liked your picture</span></h4>
-                <p class="list-group-item-text">Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.</p>
-            </a>
-            <a href="#" class="list-group-item">
-                <h4 class="list-group-item-heading" id=""><i class="fa fa-heart"></i> <span class="notification_from_who" id="">Jonas</span> <span class="notification_whathappend" id="">favorited your picture</span></h4>
-                <p class="list-group-item-text">Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.</p>
-            </a>
-            <a href="#" class="list-group-item">
-                <h4 class="list-group-item-heading" id=""><i class="fa fa-thumbs-up"></i> <span class="notification_from_who" id="">Matthias</span> <span class="notification_whathappend" id="">liked your picture</span></h4>
-                <p class="list-group-item-text">Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.</p>
-            </a>
-            <a href="#" class="list-group-item">
-                <h4 class="list-group-item-heading" id=""><i class="fa fa-thumbs-up"></i> <span class="notification_from_who" id="">Thieuke</span> <span class="notification_whathappend" id="">liked your picture</span></h4>
-                <p class="list-group-item-text">Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.</p>
-            </a>
-        </div>
+        @if(count($myNotifications) > 0)
+            <ul class="list-group">
+                @foreach($myNotifications as $notification)
 
+                    @if($notification->notificationType == 'like')
+
+                        @if($notification->user_id == Auth::user()->id)
+                            <li>You like {{ $notification->firstname }} {{ $notification->lastname }}'s project</li>
+                        @else
+                            <li>{{ $notification->firstname }} {{ $notification->lastname }} likes your project</li>
+                        @endif
+
+                    @elseif($notification->notificationType == 'favorite')
+
+                        @if($notification->user_id == Auth::user()->id)
+                            <li>You favorite {{ $notification->firstname }} {{ $notification->lastname }}'s project</li>
+                        @else
+                            <li>{{ $notification->firstname }} {{ $notification->lastname }} favorites your project</li>
+                    @endif
+
+                    @elseif($notification->notificationType == 'comment')
+
+                        @if($notification->user_id == Auth::user()->id)
+                            <li>You commented on {{ $notification->firstname }} {{ $notification->lastname }}'s project</li>
+                        @else
+                            <li>{{ $notification->firstname }} {{ $notification->lastname }} commented on your project</li>
+                        @endif
+                    @endif
+                @endforeach
+            </ul>
+        @endif
     </div>
-    <script src="{{ URL::asset('assets/js/notifications.js') }}"></script>
 @stop
