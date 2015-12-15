70314eb7cdb016844bb390b40207a72bee392a18
tommooredotbe <mooretom@outlook.com>
2015-12-15 22:39:20 +0100

    activity view

--- resources/views/UserActivity.blade.php
+++ resources/views/UserActivity.blade.php
@@ -4,28 +4,34 @@
 
     <div class="activity">
 
+        <div class="project-top">
+            <div class="container-fluid">
+                <h1>See what happend while you were away.</h1>
+            </div>
+        </div>
+
         <div class="activity_container">
 
         <h2>Your user activity</h2>
 
-        @if(count($result) < 0)
+        @if(count($result) > 0)
             <ul class="list-group">
                 @foreach($result as $notification)
 
                     @if($notification->notificationType == 'like')
 
                         @if($notification->user_id == Auth::user()->id)
-                            <li>You like {{ $notification->firstname }} {{ $notification->lastname }}'s project</li>
+                            <li>You <i class="fa fa-heart fa-fw fa-lg" id="like-icon" style="color: crimson;"></i> {{ $notification->firstname }} {{ $notification->lastname }}'s project</li>
                         @else
-                            <li>{{ $notification->firstname }} {{ $notification->lastname }} likes your project</li>
+                            <li>{{ $notification->firstname }} {{ $notification->lastname }} <i class="fa fa-heart fa-fw fa-lg" id="like-icon" style="color: crimson;"></i> your project</li>
                         @endif
 
                     @elseif($notification->notificationType == 'favorite')
 
                         @if($notification->user_id == Auth::user()->id)
-                            <li>You favorite {{ $notification->firstname }} {{ $notification->lastname }}'s project</li>
+                            <li>You <i class="fa fa-star fa-fw fa-lg" id="favorite-icon" style="color: gold;"></i> {{ $notification->firstname }} {{ $notification->lastname }}'s project</li>
                         @else
-                            <li>{{ $notification->firstname }} {{ $notification->lastname }} favorites your project</li>
+                            <li>{{ $notification->firstname }} {{ $notification->lastname }} <i class="fa fa-star fa-fw fa-lg" id="favorite-icon" style="color: gold;"></i> your project</li>
                     @endif
 
                     @elseif($notification->notificationType == 'comment')
