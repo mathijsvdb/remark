0adb6d2c0493610128f027219e896d15b23635bc
Marijn Rutten <Marijn@Marijns-MacBook-Pro.local>
2015-12-16 21:46:21 +0100

    advertising styled

--- resources/views/ads.blade.php
+++ resources/views/ads.blade.php
@@ -4,24 +4,33 @@
 
     <div class="reclam_container">
 
-
+        <div class="reclam-top">
+            <div class="container-fluid">
+                <h1>Your advertisements</h1>
+                    <a class="btn btn-primary" href="/advertising/add"><span class="glyphicon glyphicon-plus"></span>Add more</a>
+            </div>
+        </div>
 
         <div class="content_reclam col-md-12">
-            <h2>Your advertisements <a class="" href="/advertising/add">Add more +</a></h2>
+
 
 
             @if (count($myAds) > 0)
             <ul class="row list-unstyled center-block">
             @foreach($myAds as $ad)
                 <div id="{{$ad->id}}">
-                    <li class="col-md-3 col-sm-3  ad item_reclam_content">
-                        <div class="center-block wrapper_reclam">
-                            <h3>{{$ad->title}} </h3>
-                            <p>Click Counter: {{$ad->clicks}}</p>
-                            <p>{{$ad->description}} </p>
-                            <img src="{{asset($ad->img)}}" alt="reclam">
+
+                    <li class="col-md-6 col-sm-6  ad item_reclam_content">
+                        <div class="wrapper_reclam">
+                            <img src="{{asset($ad->img)}}" class="col-ms-6" alt="reclam">
+                            <div class=" info_reclam col-md-6">
+                                <h3>{{$ad->title}} </h3>
+                                <p>Click Counter: {{$ad->clicks}}</p>
+                                <p>{{$ad->description}} </p>
+                            </div>
                         </div>
                     </li>
+
                 </div>
             @endforeach
             </ul>
